4af6492069e76d5ef07cdb1e2501361da06e4047
ZakyZN <zakyzn1999@gmail.com>
2024-02-09 20:44:08 +0800

    add search filter

--- src/component/newsdata.jsx
+++ src/component/newsdata.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Card } from "react-bootstrap";
 import NavBar from "./navbar";
+import ReactLoading from "react-loading";
 
 export const NewsMain = () => {
   const [newsData, setnewsData] = useState([]);
@@ -10,7 +11,7 @@ export const NewsMain = () => {
   const handleNewsData = async (searchQuery) => {
     try {
       const url = searchQuery
-        ? `https://newsapi.org/v2/everything?q=${searchQuery}&apiKey=1f75fdb1ac9645af8d3e176292ac7907`
+        ? `https://newsapi.org/v2/everything?qInTitle=${this.props.searchQuery}&apiKey=1f75fdb1ac9645af8d3e176292ac7907`
         : `https://newsapi.org/v2/everything?q=Indonesia&apiKey=1f75fdb1ac9645af8d3e176292ac7907`;
       const res = await axios.get(url);
       //   console.log(res.data.articles);
@@ -26,35 +27,48 @@ export const NewsMain = () => {
     handleNewsData();
   }, []);
 
-  return (
-    <div className="card-container container flex" id="home">
-      <NavBar onSearch={handleNewsData} />
-      {newsData.map((articles, index) => {
-        const date = new Date(articles.publishedAt).toLocaleString("id-ID", {
-          timeZone: "Asia/Jakarta",
-        });
-        return (
-          <div id="news-card" key={articles.index} className="card">
-            <div className="card-header">
-              <img src={articles.urlToImage} alt="newsImg" id="news-img" />
-            </div>
-            <div className="card-content">
-              <h3 id="news-title">{articles.title}</h3>
-              <h6 id="news-date" className="news-date">
-                {articles.author} - {date}
-              </h6>
-              <p id="news-content" className="news-content">
-                {articles.description}
-              </p>
-              <br />
-              <Card.Link href={articles.url} className="card-link">
-                Read More
-              </Card.Link>
-            </div>
-          </div>
-        );
-      })}
-      ;
+  if (handleNewsData === 0) {
+    return (
+      <div className="card-container container flex">
+      <ReactLoading
+        type={"balls"}
+        color="#000"
+        height={"667"}
+        width={"375"}
+      />
     </div>
-  );
+    )
+  } else {
+    return (
+      <div className="card-container container flex" id="home">
+        <NavBar onSearch={handleNewsData} />
+        {newsData.map((articles, index) => {
+          const date = new Date(articles.publishedAt).toLocaleString("id-ID", {
+            timeZone: "Asia/Jakarta",
+          });
+          return (
+            <div id="news-card" key={articles.index} className="card">
+              <div className="card-header">
+                <img src={articles.urlToImage} alt="newsImg" id="news-img" />
+              </div>
+              <div className="card-content">
+                <h3 id="news-title">{articles.title}</h3>
+                <h6 id="news-date" className="news-date">
+                  {articles.author} - {date}
+                </h6>
+                <p id="news-content" className="news-content">
+                  {articles.description}
+                </p>
+                <br />
+                <Card.Link href={articles.url} className="card-link">
+                  Read More
+                </Card.Link>
+              </div>
+            </div>
+          );
+        })}
+        ;
+      </div>
+    );
+  }
 };
